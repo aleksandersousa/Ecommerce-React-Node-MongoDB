947d4043565ac511642c115ffc01f04b60936c30
aleksandersousa <aleksander-ssousa@outlook.com>
2022-01-20 17:02:12 -0300

    Created cart route

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
 
 app.listen(3002, () => {
   console.log('backend is running on port 3002');
