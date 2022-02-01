ea258afb69cd71e348efb56b49db51073db96b9f
aleksandersousa <aleksander-ssousa@outlook.com>
2022-02-01 13:27:34 -0300

    added firebase file upload

---
+++ admin-dashboard/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAe1AEDhq33A8dz6Gl9uAIhatCyAYupaHE',
+  authDomain: 'ecommerce-3e4f0.firebaseapp.com',
+  projectId: 'ecommerce-3e4f0',
+  storageBucket: 'ecommerce-3e4f0.appspot.com',
+  messagingSenderId: '673497966352',
+  appId: '1:673497966352:web:f9a0ad88bde3f0ea82a795',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- admin-dashboard/src/pages/newProduct/NewProduct.jsx
+++ admin-dashboard/src/pages/newProduct/NewProduct.jsx
@@ -1,34 +1,85 @@
 import './styles.scss';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from 'firebase/storage';
+import app from '../../firebase';
+import { addProduct } from '../../redux/apiCalls';
 
 export default function NewProduct() {
+  const [state, setState] = useState({});
+  const [file, setFile] = useState(null);
+  const [cat, setCat] = useState([]);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setState((prev) => ({
+      ...prev, [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleCat = (e) => {
+    setCat(e.target.value.split(','));
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const fileName = new Date().getTime() + file.name;
+    const storage = getStorage(app);
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          const product = { ...state, img: downloadURL, categories: cat };
+          console.log(product);
+          addProduct(dispatch, product);
+        });
+      },
+    );
+  };
+
   return (
     <div className="newProduct">
       <h1 className="addProductTitle">New Product</h1>
       <form className="addProductForm">
         <div className="addProductItem">
           <label>Image</label>
-          <input type="file" id="file" />
+          <input type="file" id="file" onChange={(e) => setFile(e.target.files[0])} />
         </div>
         <div className="addProductItem">
           <label>Title</label>
-          <input type="text" placeholder="Apple Airpods" />
+          <input name="title" type="text" placeholder="Apple Airpods" onChange={handleChange} />
         </div>
         <div className="addProductItem">
           <label>Description</label>
-          <input type="text" placeholder="Description..." />
+          <input name="desc" type="text" placeholder="Description..." onChange={handleChange} />
         </div>
         <div className="addProductItem">
           <label>Price</label>
-          <input type="text" placeholder="100" />
+          <input name="price" type="number" placeholder="100" onChange={handleChange} />
+        </div>
+        <div className="addProductItem">
+          <label>Categories</label>
+          <input type="text" placeholder="jeans,skirts..." onChange={handleCat} />
         </div>
         <div className="addProductItem">
           <label>Stock</label>
-          <select>
+          <select name="inStock" onChange={handleChange}>
             <option value="true">Yes</option>
             <option value="false">No</option>
           </select>
         </div>
-        <button type="submit" className="addProductButton">Create</button>
+        <button onClick={handleClick} type="submit" className="addProductButton">Create</button>
       </form>
     </div>
   );
